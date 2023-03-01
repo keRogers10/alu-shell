1bfbcbc1ab89ae407a77d78438961eaf47ee88c5
root <a.isingizwe@alustudent.com>
2023-03-01 07:50:54 -0800

    sfd

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0.py import add(a, b)
 def add(a,b):
-    if __name__ = __main__
-    a=1
-    b=2
-    return add(a,b)
+    if __name__ = __main__:
+        a=1
+        b=2
+        print ("{} + {} = {}".format(a,b,add(a,b)))
