29ad9d54d9e7b740104d21f2ddd4d3c4e60aab2b
root <a.isingizwe@alustudent.com>
2023-03-07 22:59:11 -0800

    The_Saint

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     length = len(my_list) - 1
-    my_list[idx] = element
     if idx < 0:
-        return None
+        return my_list
     elif idx > length:
-        return None
+        return my_list
     else:
-        return my_list[idx]
+        my_list[idx] = element
+        return my_list
