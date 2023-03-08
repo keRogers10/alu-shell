ba464ac8abcbf2a63416d204752d5b976f39d033
root <a.isingizwe@alustudent.com>
2023-03-08 09:24:33 -0800

    The_Saint

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    new = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in my_list:
+            if i > new:
+                new = i
+    return new
