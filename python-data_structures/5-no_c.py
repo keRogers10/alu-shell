9a1aa554ddd6bc68d8acd366a22d68acbf3ed58a
root <a.isingizwe@alustudent.com>
2023-03-08 02:47:13 -0800

    The_Saint

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     new = []
     for i in range(len(my_string)):
-        if my_string[i] != 'S' or my_string[i] != 's':
+        if my_string[i] != 'C' or my_string[i] != 'c':
             new.append(my_string[i])
+        else:
+            pass
     return "".join(new)
