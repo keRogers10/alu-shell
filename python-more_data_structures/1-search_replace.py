3d2d2e7b805e8f6a46ce3ea85d99d4f2f7e31243
root <a.isingizwe@alustudent.com>
2023-03-08 23:37:06 -0800

    The_Saint

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list
-    for i in new_list:
-        if i == search:
-            i = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+        else:
+            pass
     return new_list
