f7b7eb257a762cfb1c4fc5e0bd316f900602ba0d
root <a.isingizwe@alustudent.com>
2023-03-08 23:23:11 -0800

    The_Saint

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda x for x: x**2, matrix))
+    new_matrix = matrix
+    for i in new_matrix:
+        for o in i:
+            o = o**2
     return new_matrix
