610af51201f41aedb7c4555883e96d9bf4317b8f
root <a.isingizwe@alustudent.com>
2023-03-09 00:39:32 -0800

    The_Saint

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return a_dictionary.sort()
+    new = list(a_dictionary.keys())
+    now = new.sort()
+    for i in now:
+        print("{}: {}".format(i, a_dictionary.get(i)))
