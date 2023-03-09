a69f8cf8472540937406e1329872da7be1f94fa3
root <a.isingizwe@alustudent.com>
2023-03-08 23:26:20 -0800

    The_Saint

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
+    new_matrix = matrix.copy()
     for i in new_matrix:
         for o in i:
             o = o**2
