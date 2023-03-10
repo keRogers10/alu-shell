eab265d0b8a5f0bf96bfa2e3f1b3a64d193a8812
root <a.isingizwe@alustudent.com>
2023-03-10 00:50:26 -0800

    The_Saint

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return 0
-    else:
-        aadi = 0
-        count = 0
-        for i in my_list:
-            for o in i:
-                aadi += o
-                count += 1
-        return (aadi/count)
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
