a81cea101d6687ac6196cfa033413f6e0f25c5ad
root <a.isingizwe@alustudent.com>
2023-03-16 00:13:03 -0700

    The_Saint

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Write a new empty class Square that defines a square
-"""
+"""Write a class Square that defines a square by"""
 
 
 class Square:
-    """ An empty class that defines a square"""
-    def __init__(self,size=None):
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    no imporation of modules"""
+    def __init__(self, size):
         self.__size = size
