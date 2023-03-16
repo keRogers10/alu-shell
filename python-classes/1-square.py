de852bbfe7264e3532d7a017120499e38662887a
root <a.isingizwe@alustudent.com>
2023-03-16 00:10:20 -0700

    The_Saint

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Write a new empty class Square that defines a square
+"""
+
+
+class Square:
+    """ An empty class that defines a square"""
+    def __init__(self,size=None):
+        self.__size = size
