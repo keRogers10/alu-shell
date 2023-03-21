9ef24d7da4cf31b9e997d845a587cb115dd15451
root <a.isingizwe@alustudent.com>
2023-03-21 06:04:23 -0700

    The_Saint

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,8 @@ This will check if the object is an instance of a class direct or indirect
 
 
 def inherits_from(obj, a_class):
-    """ we have to specify whether the obj inserted in an instance on a class inseted
+    """ we have to specify whether the obj inserted
+    in an instance on a class inseted
 
     args:
     obj
@@ -16,4 +17,6 @@ def inherits_from(obj, a_class):
     false if it is not
     """
 
+    if type(obj) != a_class:
+        return False
     return isinstance(obj, a_class)
