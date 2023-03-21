b593fd3c550ae4937a6bcfc85486c23f2d37229a
root <a.isingizwe@alustudent.com>
2023-03-21 06:01:15 -0700

    The_Saint

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This will check if the object is an instance of a class direct or indirect
+"""
+
+
+def inherits_from(obj, a_class):
+    """ we have to specify whether the obj inserted in an instance on a class inseted
+
+    args:
+    obj
+    a_class
+
+    returns:
+    true if is an instance
+    false if it is not
+    """
+
+    return isinstance(obj, a_class)
