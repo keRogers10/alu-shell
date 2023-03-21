f9f62950c640069fdcc82a50731a1f340b68ebaa
root <a.isingizwe@alustudent.com>
2023-03-21 06:38:21 -0700

    The_Saint

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a class that will inherit characters from base geometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    we have to define some of properties
+    of rectangle based on base geometry
+
+    args:
+    width 
+    height
+    """
+
+    def __init__(self, width, height):
+        if type(width) != int or type(height) != int:
+            raise TypeError("Wrong type")
+        if width < 0 or height < 0:
+            raise ValueError("both width and height must be positive integers)
+        else:
+        self.__width = width
+        self.__height = height
