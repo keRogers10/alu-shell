5554ee234ae86628ee7a756a49ce23d154b8e899
root <a.isingizwe@alustudent.com>
2023-03-21 06:43:28 -0700

    The_Saint

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ a class that will inherit characters from base geometry"""
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """
