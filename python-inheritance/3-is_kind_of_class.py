7798a3392d3082b4771754cdbef545d4ed3e948b
root <a.isingizwe@alustudent.com>
2023-03-21 05:36:28 -0700

    The_Saint

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that checcks if the obj is an instance, of obj is instance of a parent class
+"""
+
+
+def is_kinf_of_class(obj, a_class):
+    """
+    it has to return if obj is instance of a_class
+    """
+
+    return isinstance(obj, a_class)
