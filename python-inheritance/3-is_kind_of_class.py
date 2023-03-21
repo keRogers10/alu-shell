074b58349fc4c21469aefbc8baf80e776077de7d
root <a.isingizwe@alustudent.com>
2023-03-21 05:37:19 -0700

    The_Saint

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,7 @@ a function that checcks if the obj is an instance, of obj is instance of a paren
 
 
 def is_kinf_of_class(obj, a_class):
+    
     """
     it has to return if obj is instance of a_class
     """
