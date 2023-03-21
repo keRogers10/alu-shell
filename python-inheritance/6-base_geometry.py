5b774de95572a431614b0943b89251b13174c671
root <a.isingizwe@alustudent.com>
2023-03-21 06:15:00 -0700

    The_Saint

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this is an empty class
+"""
+
+
+class BaseGeometry():
+    """
+    here we will define some functions inside a class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
