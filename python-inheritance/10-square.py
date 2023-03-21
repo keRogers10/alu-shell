4261c0a908d78d2ee60e4aae2ac26968aac5cafb
root <a.isingizwe@alustudent.com>
2023-03-21 07:36:40 -0700

    The_Saint

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,14 +4,13 @@ of rectangler
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """ a class that inherit rectangle as a parent class"""
 
     def __init__(self, size):
-        size.integer_validator('size', size)
+        size.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
 
