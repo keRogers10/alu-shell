780f489e877521e5cb80ba770209edc6a49e4398
root <a.isingizwe@alustudent.com>
2023-03-23 04:56:16 -0700

    The_Saint

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a func to append text to filename provided"""
+
+
+def append_write(filename="", text=""):
+    """a new definition to append text to filename
+    and return num of text
+    """
+
+    with open(filename, 'a') as f:
+        f.append(text)
+
+    return len(text)
