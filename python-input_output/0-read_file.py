4377c65bb5d550b5bc4229329d51fde8f6a04418
root <a.isingizwe@alustudent.com>
2023-03-23 04:30:02 -0700

    The_Saint

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ The module to open file basd on file name"""
 
+
 def read_file(filename=''):
     """new func to define our functionality"""
     with open("filename", "r", encoding="utf-8") as f:
         new = f.read()
-        print(new)
+        print(new, end="")
