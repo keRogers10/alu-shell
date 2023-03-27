4cc01ed7a0a39e8db1bad3a38ce28d7cd52e0d30
root <a.isingizwe@alustudent.com>
2023-03-27 13:04:34 -0700

    The_Saint

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- listing score above 10 only
-SELECT score, name FROM second_table ORDER BY score DESC
-WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
