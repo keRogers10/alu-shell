cf749d7554f1b9097881cdac7ae90d9afb070b59
root <a.isingizwe@alustudent.com>
2023-03-27 13:31:27 -0700

    The_Saint

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- using count
-SELECT score, COUNT(score) AS number FROM second_table ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
