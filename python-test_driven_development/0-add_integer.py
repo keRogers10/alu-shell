99eeac2dd1417c8ba3b41848f35b353484b56fad
AristideI <a.isingizwe@alustudent.com>
2023-04-18 00:44:52 -0700

    Doc  test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,10 @@ testing add_integer to check if it passes all the tests
 
 
 def add_integer(a, b=98):
+    """
+    the func to addd two int and check whether they 
+    are int or float
+    """
     if type(a) != int or type(a) != float:
         raise TypeError('a must be an integer')
     else:
